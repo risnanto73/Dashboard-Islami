93651da9c529e00f37be4d266ab2784602d54ad4
risnanto73 <tiolast63@gmail.com>
2023-10-10 09:26:16 +0700

    DI-Data Doa Update

--- app/src/main/java/com/rtid/idnsolo/dashboardislami/doa/DoaActivity.kt
+++ app/src/main/java/com/rtid/idnsolo/dashboardislami/doa/DoaActivity.kt
@@ -1,6 +1,7 @@
 
 package com.rtid.idnsolo.dashboardislami.doa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rtid.idnsolo.dashboardislami.R
@@ -12,14 +13,23 @@ class DoaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDoaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val actionBar = supportActionBar
         actionBar?.title = "Doa Harian"
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        binding = ActivityDoaBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        intentToListActivity()
 
+    }
+
+    private fun intentToListActivity() {
+        binding.cardDoaPagiMalam.setOnClickListener {
+            val intent = Intent(this, ListDoaActivity::class.java)
+            intent.putExtra(ListDoaActivity.EXTRA_TITLE, getString(R.string.pagi_dan_malam))
+            intent.putExtra(ListDoaActivity.EXTRA_IMAGE, R.drawable.ic_doa_pagi_malam)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
