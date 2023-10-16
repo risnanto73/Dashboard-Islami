af600a46e34432a693b7632dc9c3e584f7d0e5f3
risnanto73 <tiolast63@gmail.com>
2023-10-16 11:21:51 +0700

    DI-Fitur Zakat

--- app/src/main/java/com/rtid/idnsolo/dashboardislami/zakat/ZakatActivity.kt
+++ app/src/main/java/com/rtid/idnsolo/dashboardislami/zakat/ZakatActivity.kt
@@ -32,20 +32,21 @@ class ZakatActivity : AppCompatActivity() {
             val totalHarta = binding.edtTotalHartaKeseluruhan.text.toString().trim()
 
             if(totalHarta.isEmpty()) {
-                !isEmptyField
+                isEmptyField = true
                 binding.edtTotalHartaKeseluruhan.error = "Field ini tidak boleh kosong"
             }
 
             if(!isEmptyField){
 
-                if(totalHarta < emas.toString()){
+                val zakat = totalHarta.toInt() * persen
+
+                if(totalHarta > emas.toString()){
+                    binding.tvStatus.text = "Anda wajib membayar zakat"
+                    binding.tvZakatYangDikeluarkan.text = formatRupiah.format(zakat.toInt())
+
+                } else {
                     binding.tvStatus.text = "Anda belum wajib zakat"
                     binding.tvZakatYangDikeluarkan.text = "0"
-                } else {
-                    val zakat = totalHarta.toInt() * persen
-                    binding.tvStatus.text = "Anda wajib membayar zakat"
-                    //setText
-                    binding.tvZakatYangDikeluarkan.text = formatRupiah.format(zakat)
                 }
 
             }
